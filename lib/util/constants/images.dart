ad1651fd196b828028b4bad7dfa2d8f5ce8db21f
mohitambi <mohit.yadav@ambibuzz.com>
2025-08-07 00:25:33 +0530

    added assets and constants
    Signed-off-by: mohitambi <mohit.yadav@ambibuzz.com>

--- lib/util/constants/images.dart
+++ lib/util/constants/images.dart
@@ -1,29 +1,41 @@
 class Images {
   static const backButtonIcon = 'assets/icon/back_button_icon_1.png';
-  static const balanceSheetIcon = 'assets/icon/balance_sheet_icon.png';
-  static const customerLedgerIcon = 'assets/icon/customer_ledger_icon.png';
+  static const balanceSheetIcon =
+      'assets/icon/quick_links/balance_sheet_report.png';
+  static const customerLedgerIcon =
+      'assets/icon/quick_links/cust_ledger_report.png';
   static const commentIcon = 'assets/icon/comment_icon.png';
   static const downloadIcon = 'assets/icon/download_icon.png';
   static const expenseIcon = 'assets/icon/expense_icon.png';
   static const filterIcon = 'assets/icon/filter_icon.png';
   static const incomeIcon = 'assets/icon/income_icon.png';
-  static const itemsIcon = 'assets/icon/items_icon.png';
+  static const itemsIcon = 'assets/icon/quick_links/items.png';
+  static const imageNotFound = 'assets/no_image_available.png';
   static const netProfitIcon = 'assets/icon/net_profit_icon.png';
   static const noDataIcon = 'assets/icon/no_data.png';
   static const noInternetIcon = 'assets/icon/no_internet_icon.png';
   static const profileIcon = 'assets/icon/profile_icon.png';
-  static const purchaseInvoiceIcon = 'assets/icon/purchase_invoice_icon.png';
-  static const purchaseOrderIcon = 'assets/icon/purchase_order_icon.png';
+  static const purchaseInvoiceIcon =
+      'assets/icon/quick_links/purchase_invoice.png';
+  static const purchaseOrderIcon = 'assets/icon/quick_links/purchase_order.png';
   static const purchaseOrderListIcon =
       'assets/icon/purchase_order_list_icon.png';
   static const purchaseInvoiceListIcon =
       'assets/icon/purchase_order_list_icon.png';
-  static const salesInvoiceIcon = 'assets/icon/sales_invoice_icon.png';
-  static const salesOrderIcon = 'assets/icon/sales_order_icon1.png';
+  static const salesInvoiceIcon = 'assets/icon/quick_links/sales_invoice.png';
+  static const salesOrderIcon = 'assets/icon/quick_links/sales_order.png';
   static const salesOrderListIcon = 'assets/icon/sales_order_list_icon.png';
   static const salesInvoiceListIcon = 'assets/icon/sales_order_list_icon.png';
-  static const stockBalanceIcon = 'assets/icon/stock_balance_icon.png';
-  static const supplierLedgerIcon = 'assets/icon/supplier_ledger_icon.png';
+  static const stockBalanceIcon =
+      'assets/icon/quick_links/stock_balance_report.png';
+  static const supplierLedgerIcon =
+      'assets/icon/quick_links/supp_ledger_report.png';
   static const buzzItLogo = 'assets/buzzit_logo.png';
   static const poweredByAmbibuzzLogo = 'assets/powered_by_ambibuzz_logo.png';
+
+  // Drawer Icons
+  static const profileIconBlack = 'assets/icon/account_circle.png';
+  static const mainMenuIcon = 'assets/icon/apps.png';
+  static const homeIconBlack = 'assets/icon/home_icon_black.png';
+  static const customerSelectionIcon = 'assets/icon/person_search.png';
 }
